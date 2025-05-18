1f7e8a0b708bdc57f5115baa29e115d7ef5e39ca
Gabriel Santos <gabrielsantoscruz61@gmail.com>
2025-05-17 21:48:37 -0300

    ✨ feat(added server b)

---
+++ servers/server_b.py
@@ -0,0 +1,31 @@
+from global_utils import constants
+from generics import create_server
+
+# especific config for the server b
+server_config = {
+    "company": "company_b",
+    "name": "b",
+    "port": constants.SERVIDOR_B,
+    "charging_points": [
+        {
+            "id": "SE1",
+            "location": "Aracaju",
+            "capacity": 2,
+            "reserved": 0,
+            "queue": []
+        },
+        {
+            "id": "SE2",
+            "location": "Itabaiana",
+            "capacity": 1,
+            "reserved": 0,
+            "queue": []
+        }
+    ]
+}
+
+# create and execute the server
+app, port = create_server(server_config)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
